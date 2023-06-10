ef07e923a5bde4acaf0c1d6e846d38f7b6eea995
Fred Araujo <fredericoaraujo@gmail.com>
2023-06-10 14:02:03 -0300

    Changed layout edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_params, if: :devise_controller?
+  layout :layout_by_resource
 
   def after_sign_out_path_for(resource_or_scope)
-    # Customize the path or behavior after logout
     root_path
   end
   
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  private
+  def layout_by_resource
+    if devise_controller? && action_name != 'edit'
+      'login'
+    else
+      'application'
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,10 @@ module Gtd
 
     config.to_prepare do
       Devise::SessionsController.layout "login"
-      Devise::RegistrationsController.layout "login"
-      Devise::ConfirmationsController.layout "login"
-      Devise::UnlocksController.layout "login"
-      Devise::PasswordsController.layout "login"
+      # Devise::RegistrationsController.layout "login"
+      # Devise::ConfirmationsController.layout "login"
+      # Devise::UnlocksController.layout "login"
+      # Devise::PasswordsController.layout "login"
     end
   end
 end
