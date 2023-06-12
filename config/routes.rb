b608223a61a8260a6db41d9772289e6febe5d71a
Fred Araujo <fredericoaraujo@gmail.com>
2023-06-12 12:24:30 -0300

    Created models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks
+  resources :goals
+  resources :categories
+  resources :labels
   devise_for :users
   root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_014831) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_150531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.text "description"
+    t.integer "progress"
+    t.integer "total"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_groups_on_task_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_labels_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "description"
+    t.bigint "task_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_notes_on_task_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "priorities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.date "deadline"
+    t.boolean "completed"
+    t.text "context"
+    t.bigint "priority_id", default: 1, null: false
+    t.bigint "category_id"
+    t.bigint "label_id"
+    t.bigint "goal_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tasks_on_category_id"
+    t.index ["goal_id"], name: "index_tasks_on_goal_id"
+    t.index ["label_id"], name: "index_tasks_on_label_id"
+    t.index ["priority_id"], name: "index_tasks_on_priority_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -27,4 +96,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_014831) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "goals", "users"
+  add_foreign_key "groups", "tasks"
+  add_foreign_key "groups", "users"
+  add_foreign_key "labels", "users"
+  add_foreign_key "notes", "tasks"
+  add_foreign_key "notes", "users"
+  add_foreign_key "tasks", "categories"
+  add_foreign_key "tasks", "goals"
+  add_foreign_key "tasks", "labels"
+  add_foreign_key "tasks", "priorities"
 end
