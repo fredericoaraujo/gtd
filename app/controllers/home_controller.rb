512139d504a7881b332093b0f2bc0497165a0d97
Fred Araujo <fredericoaraujo@gmail.com>
2023-06-13 11:03:24 -0300

    Refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    @tasks = current_user.tasks.not_completed.order(priority_id: :desc)
+    @count_low = count_tasks(Task.priorities[:low])
+    @count_medium = count_tasks(Task.priorities[:medium])
+    @count_high = count_tasks(Task.priorities[:high])
+    @colors = { low: "teal", medium: "orange", high: "red" }
+  end
+
+  private
+  def count_tasks (priority)
+    current_user.tasks.not_completed_with_priority(priority).count
   end
 end
