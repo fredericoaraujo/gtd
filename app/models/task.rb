5983928f8f99b256868586eda050f460993cf166
Fred Araujo <fredericoaraujo@gmail.com>
2023-06-13 11:03:59 -0300

    Refactoring

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,16 @@
 class Task < ApplicationRecord
-  belongs_to :priority
-  belongs_to :category
-  belongs_to :label
-  belongs_to :goal
+  enum priority: { low: 1, medium: 2, high: 3 }
+
+  belongs_to :priority, optional: true
+  belongs_to :category, optional: true
+  belongs_to :label, optional: true
+  belongs_to :goal, optional: true
+
+  has_many :groups
+  has_many :users, through: :groups
+
+  scope :not_completed, -> { where(completed: false)}
+  scope :not_completed_with_priority, ->(priority) {
+    where(completed: false, priority_id: priority)
+  }
 end
